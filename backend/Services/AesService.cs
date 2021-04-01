80e6984907a4326029338a67558a7462b657131c
oreganoli <3611916+oreganoli@users.noreply.github.com>
2021-04-01 17:25:49 +0200

    implemented GetKey

---
+++ backend/Services/AesService.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Security.Cryptography;
+using backend.Interfaces;
+/// <summary>An implementation of <c>ISymmetricCrypto</c> using the AES algorithm.</summary>
+class AesService : ISymmetricCrypto
+{
+    Aes crypto;
+    public AesService()
+    {
+        crypto = Aes.Create();
+    }
+    public string Decode(string msg)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public string Encode(string msg)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public string GetKey()
+    {
+        crypto.GenerateKey();
+        var keyBytes = crypto.Key;
+        return BitConverter.ToString(keyBytes); // Convert to hex string
+    }
+
+    public void SetKey(byte[] key)
+    {
+        throw new System.NotImplementedException();
+    }
+}
