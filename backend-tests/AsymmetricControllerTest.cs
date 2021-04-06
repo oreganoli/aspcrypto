f9f98a03cca5724073300a7b2385b06352a01fbf
oreganoli <3611916+oreganoli@users.noreply.github.com>
2021-04-06 22:46:06 +0200

    added unit test class for asymmetric controller

---
+++ backend-tests/AsymmetricControllerTest.cs
@@ -0,0 +1,40 @@
+using System;
+using Xunit;
+using backend.Controllers;
+using backend.Services;
+
+namespace backend_tests
+{
+    public class AsymmetricControllerFixture
+    {
+        public AsymmetricController controller { get; }
+        public AsymmetricControllerFixture()
+        {
+            var svc = new RsaService();
+            controller = new AsymmetricController(svc);
+        }
+    }
+    public class AsymmetricControllerTest : IClassFixture<AsymmetricControllerFixture>
+    {
+        public AsymmetricControllerFixture fixture;
+
+        public AsymmetricControllerTest(AsymmetricControllerFixture fixture)
+        {
+            this.fixture = fixture;
+        }
+
+        /// <summary>Sanity check for encryption and decryption.</summary>
+        [Fact]
+        public void CryptoWorks()
+        {
+            var message = "The assassination is scheduled next week. I trust everyone will do their job.";
+            var ctr = fixture.controller;
+            var pair = ctr.GetKeyPair();
+            var encoded = ctr.Encode(message);
+            Assert.NotEqual(message, encoded);
+            Assert.DoesNotContain("assassination", encoded);
+            var decoded = ctr.Decode(encoded);
+            Assert.Equal(message, decoded);
+        }
+    }
+}
